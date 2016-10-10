a20c3ec0b1e4dcc6e29e31cd7b7ae60af38588ea
Nan Xiao <nan@chinadtrace.org>
2016-10-10 22:32:19 +0800

    Add wwup.go

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # The-GO-code-of-wwup
-The Go code of Working With UNIX processes.
+The Go code of [Working With UNIX Processes](http://www.jstorimer.com/products/working-with-unix-processes).
---
+++ wwup.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"os"
+)
+
+func main() {
+	/* The arguments of process */
+	fmt.Printf("There are %v arguments:\n", len(os.Args)-1)
+	for i, v := range os.Args[1:] {
+		fmt.Printf("Argument %v is %v\n", i, v)
+	}
+	fmt.Println()
+
+	/* Process and parent process ids */
+	fmt.Printf("The process id is %v.\n", os.Getpid())
+	fmt.Printf("The parent process id is %v.\n", os.Getppid())
+	fmt.Println()
+
+	/* File descriptors */
+	fmt.Printf("File descriptor of stdin is %v.\n", os.Stdin.Fd())
+	fmt.Printf("File descriptor of stdin is %v.\n", os.Stdout.Fd())
+	fmt.Printf("File descriptor of stdin is %v.\n", os.Stderr.Fd())
+	file, err := os.Open("./wwup.go")
+	if err == nil {
+		fmt.Printf("File descriptor of file is %v.\n", file.Fd())
+		file.Close()
+	}
+	fmt.Println()
+
+	/* Resource limits */
+	var olim unix.Rlimit
+	err = unix.Getrlimit(unix.RLIMIT_NOFILE, &olim)
+	if err == nil {
+		fmt.Printf("Now Current File Number Limit is %v.\n", olim.Cur)
+		fmt.Printf("Now Maximum File Number Limit is %v.\n", olim.Max)
+	}
+
+	nlim := unix.Rlimit{4096, 4096}
+	err = unix.Setrlimit(unix.RLIMIT_NOFILE, &nlim)
+	if err == nil {
+		var rlim unix.Rlimit
+		err = unix.Getrlimit(unix.RLIMIT_NOFILE, &rlim)
+		if err == nil {
+			fmt.Printf("New current File Number Limit is %v.\n", rlim.Cur)
+			fmt.Printf("New maximum File Number Limit is %v.\n", rlim.Max)
+		}
+	}
+
+	err = unix.Setrlimit(unix.RLIMIT_NOFILE, &olim)
+	if err == nil {
+		var rlim unix.Rlimit
+		err = unix.Getrlimit(unix.RLIMIT_NOFILE, &rlim)
+		if err == nil {
+			fmt.Printf("Now current File Number Limit is %v.\n", rlim.Cur)
+			fmt.Printf("Now maximum File Number Limit is %v.\n", rlim.Max)
+		}
+	}
+	fmt.Println()
+
+	/* Environment variables */
+	err = os.Setenv("FOO", "foo")
+	if err == nil {
+		fmt.Printf("The value of FOO is %v.\n", os.Getenv("FOO"))
+		os.Unsetenv("FOO")
+	}
+	fmt.Println()
+}
